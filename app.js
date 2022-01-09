27d0b791dd1c2d3d6decc511f2ab62bd54b9ef21
Ihor Rudniev <rudnievihor@gmail.com>
2022-01-09 11:45:21 +0200

    limit_json_use

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
-import { HttpCode } from "./lib/constants";
+import { HttpCode, LIMIT_JSON } from "./lib/constants";
 
 import contactsRouter from "./routes/api/contacts";
 import authRouter from "./routes/api/auth";
@@ -14,7 +14,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(helmet());
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: LIMIT_JSON }));
 
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
